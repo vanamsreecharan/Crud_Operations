9612358c19061846e127a0c6974209e5dca4a3c2
vanamsreecharan <149355094+vanamsreecharan@users.noreply.github.com>
2025-06-16 14:11:21 +0530

    Configure DB with env vars

--- db.js
+++ db.js
@@ -1,9 +1,19 @@
-const {Sequelize}=require('sequelize');
-const sequelize=new Sequelize('formcrud','root','123456',{
-host:'localhost',
-port: 3306,
+const { Sequelize } = require('sequelize');
 
-dialect:'mysql'
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true
+      }
+    }
+  }
+);
 
-});
-module.exports=sequelize;
+module.exports = sequelize;
