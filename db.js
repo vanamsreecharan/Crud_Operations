63c6f0d8be12cd48cac064795b0e0dc65f8ed5d7
vanamsreecharan <149355094+vanamsreecharan@users.noreply.github.com>
2025-06-16 15:50:12 +0530

    Switch to better-sqlite3 for Render

--- db.js
+++ db.js
@@ -17,12 +17,11 @@ const sequelize = new Sequelize(
 ); 
 
 module.exports = sequelize;*/
-// db.js
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite'
+  storage: './database.sqlite' // This file will be created automatically
 });
 
 module.exports = sequelize;
