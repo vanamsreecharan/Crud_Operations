d5f4cd9a1bcbac4f483d060d21aed1b90ab23544
vanamsreecharan <149355094+vanamsreecharan@users.noreply.github.com>
2025-06-16 14:32:09 +0530

    .env

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
-const express=require('express');
-const app=express();
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
 app.use(cors());
 
-const sequelize=require('./db');
-const userRoutes=require('./Routes/routes');
-app.use(express.static('public'));
+const sequelize = require("./db");
+const userRoutes = require("./Routes/routes");
+app.use(express.static("public"));
 app.use(express.json());
-app.use('/',userRoutes);
-app.get('/',(req,res)=>{
-res.send("your get request is working!");
+app.use("/", userRoutes);
+app.get("/", (req, res) => {
+  res.send("your get request is working!");
 });
 
-sequelize.sync({alter:true})
-.then(()=>{
-    console.log('database synchronized');
-    app.listen(3000,()=>{
-        console.log('server running at http://localhost:3000');
-    });  
-}).catch((error)=>{
-console.log('error synchronized the database',error);
-}
-);
+sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log("database synchronized");
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("error synchronized the database", error);
+  });
